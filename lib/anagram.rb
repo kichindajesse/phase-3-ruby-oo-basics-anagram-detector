5c78333dd241bf761a3b86e884d5d6fb7d9a761b
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-10 14:53:25 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
+
+anagram=Anagram.new("listen")
+anagram.match(%w(enlists google inlets banana))
+
